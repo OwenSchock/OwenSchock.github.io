a5f8e945a42295ca5075538b1c0083e52f8225ab
OwenSchock <owenschock06@gmail.com>
2021-12-10 18:23:30 +0000

    add index.html file

--- Incrementor/main.js
+++ Incrementor/main.js
@@ -24,3 +24,23 @@ window.setInterval(function(){
 	footballClick(fans);
 	
 }, 1000);
+
+
+var seasonTicketHolders = 0;
+
+function buySeasonTicketHolders(){
+    var sthCost = Math.floor(500 * Math.pow(1.1,seasonTicketHolders));     //works out the cost of this cursor
+    if(footballs >= sthCost){                                   //checks that the player can afford the cursor
+        seasonTicketHolders = seasonTicketHolders + 1;            //increases number of fans
+    	footballs = footballs - sthCost;                          //removes the footballs spent
+        document.getElementById('seasonTicketHolders').innerHTML = seasonTicketHolders;  //updates the number of fans for the user
+        document.getElementById('footballs').innerHTML = footballs;  //updates the number of footballs for the user
+    };
+    var nextCost = Math.floor(500 * Math.pow(1.1,seasonTicketHolders));       //works out the cost of the next cursor
+    document.getElementById('sthCost').innerHTML = nextCost;  //updates the cursor cost for the user
+};
+window.setInterval(function(){
+	
+	footballClick(seasonTicketHolders);
+	
+}, 1000);
